746fefcd8425be4398ca51713a15cbd5a3e1cc2d
QiFang <daolanfler@gmail.com>
2021-09-20 23:07:31 +0800

    add prismjs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,6 @@ import {VueComponent as TestMarkdown } from '../mds/test.md'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
 </script>
 
 <template>
@@ -14,19 +13,5 @@ const count = ref(0)
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
-}
 
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
 </style>
