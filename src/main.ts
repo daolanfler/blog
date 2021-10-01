02758f46b870c6193e1916e566b338c444bcecbb
QiFang <daolanfler@gmail.com>
2021-10-01 10:09:09 +0800

    scrollBehavior

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,33 @@
-import './styles/main.postcss'
-import './styles/markdown.postcss'
+import "./styles/main.postcss";
+import "./styles/markdown.postcss";
 import "./index.css";
 import "virtual:windi.css";
 
 import { ViteSSG } from "vite-ssg";
 import App from "./App.vue";
 import { routes } from "./router";
-import dayjs from 'dayjs';
-import LocalizedFormat from 'dayjs/plugin/localizedFormat'
+import dayjs from "dayjs";
+import LocalizedFormat from "dayjs/plugin/localizedFormat";
+import { RouterScrollBehavior } from "vue-router";
+
+const scrollBehavior: RouterScrollBehavior = (to, from, savedPosition) => {
+  if (savedPosition) {
+    return savedPosition;
+  } else {
+    return {
+      top: 0,
+    };
+  }
+};
 
 export const createApp = ViteSSG(
   // the root component
   App,
   // vue-router options
-  { routes },
+  { routes, scrollBehavior },
   // function to have custom setups
   ({ app, router, routes, isClient, initialState }) => {
     // install plugins etc.
-    dayjs.extend(LocalizedFormat)
+    dayjs.extend(LocalizedFormat);
   }
 );
