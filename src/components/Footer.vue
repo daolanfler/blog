201bf5bc28b310d7783e2f5111d4731ba7829248
daolanfler <daolanfler@gmail.com>
2022-05-19 21:01:34 +0800

    lint: format

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,8 @@ import heartIcon from '@iconify-icons/el/heart.js'
 <template>
   <footer class="py-5 h-16">
     <div class="copyright flex items-center justify-center ">
-      <span class="dark:text-gray-300">&copy; 2022</span>&nbsp;<Icon :icon="heartIcon" class="text-red-600" />
+      <span class="dark:text-gray-300">&copy; 2022</span>&nbsp;
+      <Icon :icon="heartIcon" class="text-red-600" />
       &nbsp;<span>daolanfler</span>
     </div>
   </footer>
