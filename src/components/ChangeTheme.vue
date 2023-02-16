6a502770e480836704d9c7106cc0b60820a0e64e
daolanfler <daolanfler@gmail.com>
2023-02-16 15:25:59 +0800

    wip: update about page

--- src/components/ChangeTheme.vue
+++ src/components/ChangeTheme.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue/dist/offline.js'
-import iconDark from '@iconify-icons/ic/outline-dark-mode.js'
-import iconLight from '@iconify-icons/ic/outline-light-mode.js'
+import Dark from '@iconify-icons/ic/outline-dark-mode.js'
+import Light from '@iconify-icons/ic/outline-light-mode.js'
 import { isDark, toggleDark } from '../store'
 
 </script>
@@ -11,13 +11,13 @@ import { isDark, toggleDark } from '../store'
     <Icon
       v-if="isDark"
       class="text-2xl cursor-pointer"
-      :icon="iconDark"
+      :icon="Dark"
       @click="toggleDark()"
     />
     <Icon
       v-else
       class="text-2xl cursor-pointer"
-      :icon="iconLight"
+      :icon="Light"
       @click="toggleDark()"
     />
   </div>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue/dist/offline.js'
-import heartIcon from '@iconify-icons/el/heart.js'
+import Heart from '@iconify-icons/el/heart.js'
 </script>
 
 <template>
   <footer class="py-5 h-16">
     <div class="copyright flex items-center justify-center ">
-      <span class="dark:text-gray-300">&copy; 2022</span>&nbsp;
-      <Icon :icon="heartIcon" class="text-red-600" />
+      <span class="dark:text-gray-300">&copy; 2022~2023</span>&nbsp;
+      <Icon :icon="Heart" class="text-red-600" />
       &nbsp;<span>daolanfler</span>
     </div>
   </footer>
