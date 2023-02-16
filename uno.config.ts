64be1c795e5fd7f978f96ebb0128708d2ff82c3a
daolanfler <daolanfler@gmail.com>
2023-02-16 14:18:49 +0800

    chore: update deps

--- uno.config.ts
+++ uno.config.ts
@@ -7,5 +7,8 @@ export default defineConfig({
   transformers: [transformerDirectives()],
   presets: [
     presetUno()
-  ]
+  ],
+  shortcuts: {
+    "code": "px-1 rounded-sm dark:(text-red-500 bg-dark-200) text-red-500 bg-light-700"
+  }
 })
