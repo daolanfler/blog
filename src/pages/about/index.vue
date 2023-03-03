5015fe2f91fd75ec93d79bc95c630db7b20f39a4
daolanfler <daolanfler@gmail.com>
2023-03-03 17:57:38 +0800

    chore: udpate about

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -1,30 +1,65 @@
 <template>
-  <div class="h-[calc(100vh-16em)] flex flex-col justify-center items-center">
-    <div class="flex items-center space-x-6 flex-wrap">
-      <span class="site-link" @click="jumpTo('twitter')">
-        <Icon icon="mdi:twitter" class="text-[#1a8cd8] w-12 h-12" />
-        <span class="font-mono text-[12px]">Twitter</span>
-      </span>
-      <span class="site-link" @click="jumpTo('github')">
-        <Icon icon="mdi:github" class="w-12 h-12" />
-        <span class="font-mono text-[12px]">GitHub</span>
-      </span>
-      <span class="site-link" @click="jumpTo('onlyfans')">
-        <!-- <img
-          src="../../assets/icons/512px-OnlyFans_Logo.svg.png"
-          class="h-12"
-          alt="onlyfans icon"
-        > -->
-        <Icon icon="tabler:brand-onlyfans" class="text-[#1a8cd8] w-12 h-12" />
-        <span class="self-start font-mono text-[12px]">OnlyFans</span>
+  <div class="dark:text-gray-300">
+    <div class="mb-6">
+      <p class="text-lg font-bold mb-2">Whoami ?</p>
+      <p>A wanderer by trade, a fontend developer</p>
+      <p>Randomly using mac/linux/window and feels all ok</p>
+      <p>Recently been learning rust (wish me good luck)</p>
+      <p>Enthusiast of rock'n'roll, folk & metal music</p>
+      <p>Insisting freedom of speech and liberalisum</p>
+      <p>Will be in many corners of this planet</p>
+    </div>
+    <div class="mb-6">
+      <p class="text-lg font-bold mb-2">Things that make life easier</p>
+      <ul>
+        <li>Vue3 + Vite</li>
+        <li>Leaflet</li>
+        <li>Rxjs</li>
+        <li>D3</li>
+        <li>WSL2 on windows</li>
+        <li>VS Code + Vim extension</li>
+      </ul>
+    </div>
+    <div class="mb-6">
+      <p class="text-lg font-bold mb-2">Demo & Others</p>
+      <span class="inline-block hover:underline">
+        <router-link
+          :to="{ path: '/gallery' }"
+          class="dark:hover:text-white hover:text-blue"
+        >
+          visit here {{ ">>" }}
+        </router-link>
       </span>
     </div>
-    <div
-      class="mt-8 text-3xl text-gray-400 dark:hover:text-white hover:(text-blue underline)"
-    >
-      <router-link :to="{ path: '/gallery' }" class="font-mono">
-        Others
-      </router-link>
+
+    <div>
+      <p class="text-lg font-bold mb-2">Find me here</p>
+      <div class="flex items-center space-x-6 flex-wrap">
+        <a href="mailto:daolanfler@gmail.com">
+          <span class="site-link">
+            <Icon icon="logos:google-gmail" class="text-[#1a8cd8] w-8 h-8" />
+            <span class="font-mono text-[12px]">Gmail</span>
+          </span>
+        </a>
+        <a href="https://twitter.com/daolanfler">
+          <span class="site-link">
+            <Icon icon="mdi:twitter" class="text-[#1a8cd8] w-8 h-8" />
+            <span class="font-mono text-[12px]">Twitter</span>
+          </span>
+        </a>
+        <a href="https://github.com/daolanfler">
+          <span class="site-link">
+            <Icon icon="mdi:github" class="w-8 h-8" />
+            <span class="font-mono text-[12px]">GitHub</span>
+          </span>
+        </a>
+        <a href="https://discord.gg/ukNc3UJZxE">
+          <span class="site-link">
+            <Icon icon="ic:baseline-discord" class="w-8 h-8" />
+            <span class="self-start font-mono text-[12px]">Discord</span>
+          </span>
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -34,14 +69,17 @@
 // lint staged
 function jumpTo(place: string) {
   switch (place) {
+    // case "gmail":
+    //   window.open("mailto:daolanfler\+blog@gmail.com");
+    //   break;
     case "twitter":
       window.location.href = "https://twitter.com/daolanfler";
       break;
     case "github":
       window.location.href = "https://github.com/daolanfler";
       break;
-    case "onlyfans":
-      window.location.href = "https://www.bilibili.com/video/BV1GJ411x7h7";
+    case "discord":
+      window.location.href = "https://discord.gg/ukNc3UJZxE";
       break;
     default:
       break;
